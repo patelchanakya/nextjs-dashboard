a0a17852fd23a3f3560dcea0874d971da7fb425a
c_patel <chanakya1patel@gmail.com>
2024-02-07 00:30:10 -0700

    adding desktop hero image

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,13 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
